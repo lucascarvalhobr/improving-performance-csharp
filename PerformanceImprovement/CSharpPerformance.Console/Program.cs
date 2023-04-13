16126d84018ed58d10efafedf54c8c6d45f89062
Lucas Carvalho <lucascarvalho34@gmail.com>
2023-04-13 17:33:15 -0300

    chore: some more files

---
+++ PerformanceImprovement/CSharpPerformance.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CSharpPerformance.Code;
+
+var database = new Database("Data Source=LAPTOP-3G4SAB89\\SQLEXPRESS;Initial Catalog=Samples;Integrated Security=True;TrustServerCertificate=true");
+database.GetPersonByName("Bruno");
+
+Console.ReadKey();
